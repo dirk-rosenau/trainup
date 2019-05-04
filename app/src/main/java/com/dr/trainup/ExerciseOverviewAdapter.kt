0e634882f7340c2a95e562e47c6ab697ec5fce58
Dirk Rosenau <d.rosenau@thalia.de>
2019-05-04 21:36:32 +0200

    WIP: Started exercise overview ui.

---
+++ app/src/main/java/com/dr/trainup/ExerciseOverviewAdapter.kt
@@ -0,0 +1,12 @@
+package com.dr.trainup
+
+import androidx.recyclerview.widget.RecyclerView
+import com.dr.data.entities.Exercise
+
+class ExerciseOverviewAdapter : RecyclerView.Adapter<Exercise> {
+
+
+    class ExerciseViewHolder : RecyclerView.ViewHolder {
+        //val binding = ColorItemBinding.bind(itemView)
+    }
+}

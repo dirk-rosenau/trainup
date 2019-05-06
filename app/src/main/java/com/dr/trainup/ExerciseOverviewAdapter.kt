76fb4721d4adb650b46bef3ff811985571bb42bb
Dirk Rosenau <dirk@skillhero.de>
2019-05-06 21:22:31 +0200

    Playing... maybe rebase

--- app/src/main/java/com/dr/trainup/ExerciseOverviewAdapter.kt
+++ app/src/main/java/com/dr/trainup/ExerciseOverviewAdapter.kt
@@ -1,12 +1,25 @@
 package com.dr.trainup
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dr.data.entities.Exercise
 
-class ExerciseOverviewAdapter : RecyclerView.Adapter<Exercise> {
+class ExerciseOverviewAdapter(private val exercises: List<Exercise>) :
+    RecyclerView.Adapter<ExerciseOverviewAdapter.ExerciseViewHolder>() {
+    override fun getItemCount(): Int = exercises.size
 
 
-    class ExerciseViewHolder : RecyclerView.ViewHolder {
+    override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class ExerciseViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
         //val binding = ColorItemBinding.bind(itemView)
     }
 }
---
+++ app/src/main/java/com/dr/trainup/ExerciseOverviewItemViewModel.kt
@@ -0,0 +1,5 @@
+package com.dr.trainup
+
+class ExerciseOverviewItemViewModel {
+
+}

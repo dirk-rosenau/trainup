b6841e02a23f93f811184a5cebd7335e8494a2d2
Dirk Rosenau <d.rosenau@thalia.de>
2019-05-20 15:05:15 +0200

    Cleanup: Removed fragment Injector from activity

--- app/src/main/java/com/dr/trainup/TrainUpApplication.kt
+++ app/src/main/java/com/dr/trainup/TrainUpApplication.kt
@@ -2,29 +2,23 @@ package com.dr.trainup
 
 import android.app.Activity
 import android.app.Application
-import androidx.fragment.app.Fragment
 import com.dr.trainup.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
-import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-class TrainUpApplication : Application(), HasActivityInjector, HasSupportFragmentInjector {
+class TrainUpApplication : Application(), HasActivityInjector {
 
     @Inject
     lateinit var activityInjector: DispatchingAndroidInjector<Activity>
 
-    @Inject
-    lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
-
     override fun onCreate() {
         super.onCreate()
-         DaggerAppComponent.builder().application(this).build().inject(this)
+        DaggerAppComponent.builder().application(this).build().inject(this)
     }
 
     override fun activityInjector(): AndroidInjector<Activity> = activityInjector
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentInjector
 }
 
